a82895e2c05a35b99304db40a5371101cd3a2de7
David <viddyshep@gmail.com>
2018-06-26 22:44:53 -0400

    finaly finished

---
+++ click game/src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = props => (
+    <header className="header">
+        <h1 id="shadow">Pokémon Click Quest!</h1>
+        <h2 id="shadow">Click on an image to earn points, but don't click on any more than once!</h2>
+    </header>
+);
+export default Header;
